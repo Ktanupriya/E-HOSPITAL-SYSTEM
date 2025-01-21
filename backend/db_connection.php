81f04fa6d086a586a609cfdffa8fed5206277171
Ktanupriya <kambletanupriya@gmail.com>
2025-01-22 00:03:46 +0530

    Update db_connection.php

--- backend/db_connection.php
+++ backend/db_connection.php
@@ -1,57 +1,12 @@
 <?php
+$host = "sql213.infinityfree.com";  // Example from InfinityFree
+$username = "if0_38151079";
+$password = "Ktanu2024";
+$dbname = "if0_38151079_XXX";
 
+$conn = new mysqli($host, $username, $password, $dbname);
 
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db = 'e_hospital';
-
-// Establish a connection
-$conn = new mysqli($host, $user, $pass, $db);
-
-// Check the connection
 if ($conn->connect_error) {
-    die("Failed to connect to the database: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// $host = 'localhost';
-// $user = 'root';
-// $pass = '';
-// $db = 'e_hospital';
-
-// $conn = mysqli($host,$user,$pass,$db);
-
-// if ($conn->connect_error) {
-//     echo "Failed to connect DB".$conn->connect_error;
-// }
-// echo "Database connected successfully!";
-
-
-// if (isset($_POST)['submit']) {
-//     $username = $_POST['username'];
-//     $password = $_POST['password'];
-
-
-//     $host = 'localhost';
-//     $user = 'root';
-//     $pass = '';
-//     $dbname = 'e_hospital';
-
-//     $conn = mysqli_connect($host,$user,$pass,$dbname);
-
-//     $sql = "INSERT INTO login(username,password) values ('$username','$password')";
-//     mysqli_query($conn,$sql);
-// }
 ?>
-
